f04af5257b08ed324fa4b761b11cde5190248215
tor <semkulich.m@gmail.com>
2020-11-15 13:48:51 +0200

    Task 2: Webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,71 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
 
-module.exports = {
+module.exports = merge(commonConfig, {
     mode: 'development',
-    entry: ['./src/index.js'],
-    output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dev')
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, './src'), 'node_modules'],
-        extensions: ['.js', '.jsx', '.json']
-    },
-    watch: true,
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'dev'),
-        compress: true,
-        port: 9000,
-        open: true
+        contentBase: './dist',
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dev']),
-        new HtmlWebpackPlugin({
-            title: 'Test App',
-            template: 'src/index.html',
-            filename: 'index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[name].css'
-        })
-    ],
     module: {
         rules: [
             {
-                enforce: 'pre',
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: 'eslint-loader'
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             },
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: { attrs: false }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    'css-loader',
-                    'sass-loader'
-                ],
-            }
         ]
     }
-};
+})
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,77 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+
+module.exports = merge(commonConfig, {
     mode: 'production',
-    entry: ['./src/index.js'],
-    output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, './src'), 'node_modules'],
-        extensions: ['.js', '.jsx', '.json']
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'build'),
-        compress: true,
-        port: 8000,
-        open: true
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(['build']),
-        new HtmlWebpackPlugin({
-            title: 'Test App',
-            template: 'src/index.html',
-            filename: 'index.html',
-            minify: { collapseWhitespace: true }
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[name].css'
-        })
-    ],
+    devtool: 'source-map',
     module: {
         rules: [
             {
-                enforce: 'pre',
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: 'eslint-loader'
-            },
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
+                test: /\.css$/,
                 use: [
-                    {
-                        loader: 'html-loader',
-                        options: { attrs: false }
-                    }
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'sass-loader'
                 ],
-            }
-        ]
-    }
-};
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ]
+})
